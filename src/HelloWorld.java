9502bd0cbe8daab8d67b110da906247cdab57be7
mikerurk <mike-fo@yandex.ru>
2019-04-14 17:08:56 +0300

    commit in macbook1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi! Bro..11 macbook");
+        System.out.println("Hi! Bro..11 macbook!");
     }
 }
